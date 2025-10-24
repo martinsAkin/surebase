28da76a62014a0d80c76198f7b01b398553a4c62
veli <143023447+Velination@users.noreply.github.com>
2025-10-24 18:48:27 +0100

    Ife (#7)
    * setup
    
    * new setup for surebase
    
    * pages created
    
    created signup page , login page ,privacy page ,terms of use page and faq page
    
    * fixed the conflixt
    
    * fixed mobile views of this pages (faq, privacy, login, signup, term of use  )
    
    made it mobile responsive

--- surebase/src/app/app.ts
+++ surebase/src/app/app.ts
@@ -1,30 +1,14 @@
 // src/app/app.ts
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
-import { Footer } from './component/footer/footer';
-import { Headbar } from './component/headbar/headbar';
-import { Router } from '@angular/router'
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    Headbar,
-    RouterOutlet,
-    RouterLinkWithHref,
-    RouterLinkActive,
-    Footer
-],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',    // <-- points to the file you have
+  styleUrls: ['./app.scss'],              // or remove if you don't have app.scss
 })
 export class AppComponent {
-  constructor(public router: Router){}
-
-  get hideLayout(){
-    const hiddenRoutes = ['/login', '/signup']
-    return hiddenRoutes.includes(this.router.url)
-  }
+  title = 'surebase-ui';
 }
--- surebase/src/app/pages/faq/faq.component.ts
+++ surebase/src/app/pages/faq/faq.component.ts
@@ -27,7 +27,7 @@ export class FaqComponent {
     },
     {
       key: 'insurers',
-      label: 'For Insurance Companies',
+      label: 'Insurance Companies',
       items: [
         { q: 'How do we distribute products on Surebase?', a: 'Publish products through our partner console and gain access to our network of vetted distributors.' },
         { q: 'How are rates & rules managed?', a: 'Via the console or API; changes are versioned and auditable.' },
@@ -35,7 +35,7 @@ export class FaqComponent {
     },
     {
       key: 'enterprises',
-      label: 'For Brokers & Corporate',
+      label: 'Brokers & Corporate',
       items: [
         { q: 'Can we white-label?', a: 'Yes, configurable branding and hosted flows are available.' },
       ],
