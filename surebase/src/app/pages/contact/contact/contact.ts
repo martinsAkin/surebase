60b359f00419f02b8e9ad997214873789597f091
deeloins <114101485+deeloins@users.noreply.github.com>
2025-10-24 18:48:13 +0100

    Refactor contact page components and update styles for responsiveness (#8)

--- surebase/src/app/pages/contact/contact/contact.ts
+++ surebase/src/app/pages/contact/contact/contact.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactHeroComponent } from "../contact-hero/contact-hero";
 import { ContactFormComponent } from "../contact-form/contact-form";
-import { CtaComponent } from '../cta/cta';
+import { SimplifyCard } from "../../../component/simplify-card/simplify-card";
 
 @Component({
   selector: 'app-contact',
-  imports: [ContactHeroComponent, ContactFormComponent, CtaComponent ],
+  imports: [ContactHeroComponent, ContactFormComponent, SimplifyCard],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
