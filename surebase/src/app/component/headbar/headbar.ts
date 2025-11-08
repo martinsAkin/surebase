c9793d74518e32456e17c0db61ad4eaf56c3fdf7
martinsAkin <martinsakin98@gmail.com>
2025-11-08 14:53:57 +0100

    sidebar integrated

--- surebase/src/app/component/headbar/headbar.ts
+++ surebase/src/app/component/headbar/headbar.ts
@@ -1,37 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive }from '@angular/router'
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-headbar',
   standalone: true,
   imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './headbar.html',
-  styleUrl: './headbar.scss'
+  styleUrl: './headbar.scss',
 })
 export class Headbar {
-    isProductOffering = false;
-    isDevelopers = false;
+  isProductOffering = false;
+  isDevelopers = false;
 
-    toggeleProductOffering(){
-      this.isProductOffering = !this.isProductOffering
-      this.isDevelopers = false;
-    }
+  toggeleProductOffering() {
+    this.isProductOffering = !this.isProductOffering;
+    this.isDevelopers = false;
+  }
 
-    toggleDevelopers() {
-  this.isDevelopers = !this.isDevelopers;
-  this.isProductOffering = false; 
-   }
+  toggleDevelopers() {
+    this.isDevelopers = !this.isDevelopers;
+    this.isProductOffering = false;
+  }
 
-    closeProductOffering(){
-      this.isDevelopers = false;
-      this.isProductOffering = false;
-    }
+  closeProductOffering() {
+    this.isDevelopers = false;
+    this.isProductOffering = false;
+  }
 
-    isMenuOpen = false;
-    toggleMenuOpen(){
-      this.isMenuOpen = !this.isMenuOpen
-    }
-    toggleMenuClose(){
-      this.isMenuOpen = false;
-    }
+  isMenuOpen = false;
+  toggleMenuOpen() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  toggleMenuClose() {
+    this.isMenuOpen = false;
+  }
 }
